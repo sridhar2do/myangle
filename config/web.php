f470b98480cbe5cb1674d3f3c8c6b98c077fdf92
Sridhar M <sridhar2do@gmail.com>
2016-05-11 14:01:10 +0530

    Update cookie and file permissions

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'sdflj#)(OPK;LSDKF0)(#$*)()w(eifplkjdwJLWGW',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
